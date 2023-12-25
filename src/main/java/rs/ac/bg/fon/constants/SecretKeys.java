99f1f5bc83f80bb254efe2c4a503ccf6b4af69b8
janko <nikola.jankovic@cbs.co.rs>
2023-12-25 15:52:45 +0100

    Updating get Algorithm method

--- src/main/java/rs/ac/bg/fon/constants/SecretKeys.java
+++ src/main/java/rs/ac/bg/fon/constants/SecretKeys.java
@@ -30,6 +30,19 @@ public class SecretKeys {
     }
 
     public static Algorithm getAlgorithm() {
-        return Algorithm.HMAC256(secretFile.getProperty(jwt_secret_key_field).getBytes());
+        if(secretFile.isEmpty()){
+            try {
+                secretFile.load(new FileInputStream(Constants.RESOURCE_FOLDER + SECRET_FILE));
+                logger.info("Successfully loaded secret properties");
+            } catch (Exception e) {
+                logger.error("Exception: Error loading secret properties");
+            }
+        }
+        String property = secretFile.getProperty(jwt_secret_key_field);
+        if(property==null){
+            logger.error("Exception: Error loading property for Algorithm!");
+            return null;
+        }
+        return Algorithm.HMAC256(property.getBytes());
     }
 }
