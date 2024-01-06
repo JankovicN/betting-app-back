6380ff2a08f6b2f9dd5b44eddf2ce3d8f686619b
JankovicN <nj20180199@student.fon.bg.ac.rs>
2024-01-06 16:43:42 +0100

    Renaming scheduler class

--- src/main/java/rs/ac/bg/fon/scheduler/TicketUpdaterScheduler.java
+++ src/main/java/rs/ac/bg/fon/scheduler/TicketUpdaterScheduler.java
@@ -1,8 +1,9 @@
-package rs.ac.bg.fon.job;
+package rs.ac.bg.fon.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import rs.ac.bg.fon.service.BetService;
@@ -11,50 +12,51 @@ import rs.ac.bg.fon.service.TicketService;
 
 @RequiredArgsConstructor
 @Component
-public class TicketUpdaterJob {
+public class TicketUpdaterScheduler {
 
-    private static final Logger logger = LoggerFactory.getLogger(TicketUpdaterJob.class);
+    private static final Logger logger = LoggerFactory.getLogger(TicketUpdaterScheduler.class);
     private final BetService betService;
     private final TicketService ticketService;
     private final FootballApiService footballApiService;
-
+    @Async
     @Scheduled(initialDelay = 10 * 1000, fixedRate = 60 * 60 * 1000)
-    public void updateBets() {
+    public void updateBetsJob() {
         try {
             betService.updateAllBets();
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
     }
-
+    @Async
     @Scheduled(initialDelay = 2 * 60 * 1000, fixedRate = 60 * 60 * 1000)
-    public void payoutUsers() {
+    public void payoutUsersJob() {
         try {
             ticketService.payoutUsers();
         } catch (Exception e) {
             logger.error("Error while paying out users! \n" + e.getMessage());
         }
     }
-
+    @Async
     @Scheduled(initialDelay = 30 * 1000, fixedRate = 60 * 60 * 1000)
-    public void updateTickets() {
+    public void updateTicketsJob() {
         try {
             ticketService.updateAllTickets();
         } catch (Exception e) {
             logger.error("Error while updating ticket! \n" + e.getMessage());
         }
     }
-
+    @Async
     @Scheduled(initialDelay = 1000, fixedRate = 1000 * 60)
-    public void processTickets() {
+    public void processTicketsJob() {
         try {
             ticketService.processTickets();
         } catch (Exception e) {
             logger.error("Error while updating ticket! \n" + e.getMessage());
         }
     }
+    @Async
     @Scheduled(initialDelay = 1000 * 60 * 60 * 12, fixedRate = 1000 * 60 * 60 * 24)
-    public void fetchNewFixtures() {
+    public void fetchNewFixturesJob() {
         try {
             footballApiService.getFixturesAndOddsFromAPI();
         } catch (Exception e) {
